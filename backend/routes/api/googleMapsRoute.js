36911c1f77ef5111bd065ca02ac580968b6f309d
IdotEXE-313 <mcpegamerxx1@gmail.com>
2022-11-24 18:43:48 +0000

    added schools db

--- backend/routes/api/googleMapsRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../../controllers/api/googleMaps");
-
-router
-    .route("/api/google")
-    .get(controller.getNearbySchools)
-
-module.exports = router;
