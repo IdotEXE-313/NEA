805e467ca66ef49428eff15011c2fd97008ef5cd
IdotEXE-313 <mcpegamerxx1@gmail.com>
2022-12-13 15:51:19 +0000

    added confirmation card to subject buttons

--- frontend/revisionappnea/src/screens/addSubject/addSubject.js
+++ frontend/revisionappnea/src/screens/addSubject/addSubject.js
@@ -3,11 +3,22 @@ import NavigationBar from "../../components/navigation/navigationbar";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/esm/Button";
+import Backdrop from "@mui/material/Backdrop";
+import Card from 'react-bootstrap/Card';
 import './addSubject.css';
 
 const AddSubject = () => {
 
     const[subjects, setSubjects] = useState([]);
+    const[open, setOpen] = useState(false);
+    const[subject, setSubject] = useState("");
+
+    const handleOpen = (event) => {
+        setOpen(true);
+        setSubject(event.target.value);
+
+    }
+    const handleClose = () => setOpen(false);
 
     useEffect(() => {
         const fetchSubjects = async () => {
@@ -27,15 +38,30 @@ const AddSubject = () => {
     return(
         <>
             <NavigationBar />
+            <div className="info-container">
+                <h1 className="heading">Add A Subject</h1>
+                <p className="info-text">Any subjects added here will be displayed in the subjects tab</p>
+            </div>
             <div className="subjects-container">
                     {subjects.map((subject) => {
                         return(
-                            <Button variant="primary" className="subject-button" key={subject.SubjectID} >
+                            <Button variant="primary" className="subject-button" key={subject.SubjectID} onClick={handleOpen} value={subject.SubjectName} >
                                 {subject.SubjectName}
                             </Button>
                         )
                     })}
             </div>
+            <Backdrop open={open} >
+                <Card className="confirm-card">
+                    <Card.Title>
+                        Add {subject} To Your Subjects?
+                    </Card.Title>
+                    <div className="buttons-container">
+                        <Button className="select-button" variant="info">Yes</Button>
+                        <Button className="select-button" variant="info" onClick={handleClose}>No</Button>
+                    </div>
+                </Card>
+            </Backdrop>
 
         </>
     )
