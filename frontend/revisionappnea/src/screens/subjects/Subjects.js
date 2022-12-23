15679e2efc81c5adef53e45557df1a30ad7baf69
IdotEXE-313 <mcpegamerxx1@gmail.com>
2022-12-23 17:45:03 +0000

    updated backend to reflect change from subject tbl

--- frontend/revisionappnea/src/screens/subjects/Subjects.js
+++ frontend/revisionappnea/src/screens/subjects/Subjects.js
@@ -9,7 +9,6 @@ import { useNavigate } from "react-router-dom";
 const Subjects = () => {
 
     const[subjects, setSubjects] = useState([]);
-    const navigate = useNavigate();
 
     useEffect(() => {
         const getSubjectsTaken = async () => {
@@ -23,11 +22,6 @@ const Subjects = () => {
         }
         getSubjectsTaken();
     }, []);
-
-    const generatePath = (subject) => {
-        const subjectName = subject.replace(" ", "-");
-        return `/subjects/${subjectName}/`;
-    }
 
     return(
         <>
@@ -43,7 +37,7 @@ const Subjects = () => {
                             <Card.Body>
                                 <Card.Title>{subject.SubjectName}</Card.Title>
                             </Card.Body>
-                            <Button onClick={() => navigate(generatePath(subject.SubjectName))}>Decks</Button>
+                            <Button>Decks</Button>
                         </Card>
                     )
                 })}
