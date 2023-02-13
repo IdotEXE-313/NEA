08f4bb8bc84159747bfa7b7e76495e5adf1081ee
IdotEXE-313 <mcpegamerxx1@gmail.com>
2023-02-13 16:24:02 +0000

    actually fixed the bug this time...

--- backend/controllers/insertData/addCardController.js
+++ backend/controllers/insertData/addCardController.js
@@ -1,12 +1,14 @@
 const db = require("../../database/connection");
+const dateObject = new Date();
 
 exports.addCard = async (req, res) => {
     const {cardFront, cardBack, deckID} = req.body;
     const efactor = 2.5; //this is the default efactor for new cards
+    const date = new Date(dateObject.getFullYear(), dateObject.getMonth(), dateObject.getDate());
     const cardID = Math.floor(Date.now() + Math.random());
 
-    await db.query("INSERT INTO card (cardID, deckID, CardFront, CardBack, intervalNum, repitition, efactor) VALUES (?, ?, ?, ?, ?, ?, ?)", 
-        [cardID, deckID, cardFront, cardBack, 0, 0, efactor])
+    await db.query("INSERT INTO card (cardID, deckID, CardFront, CardBack, NextReviewDate, intervalNum, repitition, efactor) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
+        [cardID, deckID, cardFront, cardBack, date, 0, 0, efactor])
         .then((response) => {
             res.send({success: "Card Added"})
         })
