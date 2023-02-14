d79ce7a9e9e702cb40148d473643eab2c356b017
IdotEXE-313 <mcpegamerxx1@gmail.com>
2023-02-14 16:29:56 +0000

    bug: first and last cards fetched cause crash

--- backend/controllers/fetchData/getCardDataControllerQueue.js
+++ backend/controllers/fetchData/getCardDataControllerQueue.js
@@ -14,7 +14,7 @@ exports.queueCards = async(req, res) => {
         })
     }
 
-    await db.query("SELECT CardID, Priority, CardFront, CardBack FROM card WHERE card.DeckID = ? AND card.Priority IS NOT NULL", [deckID])
+    await db.query("SELECT CardID, DeckID, Priority, CardFront, CardBack FROM card WHERE card.DeckID = ? AND card.Priority IS NOT NULL", [deckID])
     .then((response) => {
         console.log(response);
         //  receives response in form {CardFront:, CardBack:}
--- frontend/revisionappnea/src/screens/reviewPage/ReviewQueue.js
+++ frontend/revisionappnea/src/screens/reviewPage/ReviewQueue.js
@@ -9,23 +9,22 @@ import Button from 'react-bootstrap/Button';
 const ReviewQueue = () => {
 
     const deckID = useParams();
+    const[cardData, setCardData] = useState([]);
     const[cardBack, setCardBack] = useState("");
     const[cardFront, setCardFront] = useState("");
     const[revealBack, setRevealBack] = useState(false);
     const[visibilityReveal, setVisiblityReveal] = useState("");
     const[visibilityOptions, setVisibilityOptions] = useState("d-none");
-    const[priority, setPriority] = useState(0);
-    const[cardID, setCardID] = useState("");
 
 
     useEffect(() => {
         const queueCards = async() => {
-            await axios.post("http://localhost:3001/card-data-queue", {
+            await axios.post("http://localhost:3001/card-data-queue", { //adds cards onto the queue
                 withCredentials: true,
                 deckID: deckID.deckid
             })
-            .then((res) => {
-                dequeueCard();
+            .then(() => {
+                dequeueCard(); //we dequeuea card here to get out first card off the created queue
             })
             .catch((err) => {
                 console.log(err);
@@ -35,34 +34,45 @@ const ReviewQueue = () => {
     }, [])
 
 
-    const dequeueCard = async (newPriority) => {
+    const dequeueCard = async () => {
         await axios.get("http://localhost:3001/card-data-queue")
             .then((res) => {
                 console.log(res);
-                setVisibilityOptions("d-none");
-                setRevealBack(false);
-                setVisiblityReveal("");
-                if(res.data.cardData!== null){
-                    setCardBack(res.data.cardData.value.CardBack);
-                    setCardFront(res.data.cardData.value.CardFront);
-                    setCardID(res.data.cardData.value.CardID);
-                    setPriority(res.data.cardData.value.Priority);
+                setCardData(res.data.cardData);
+                showCardFront();
+
+                if(cardData !== null){
+                    setCardBack(cardData.value.CardBack); //separate variables since their state needs to differ from their initial values
+                    setCardFront(cardData.value.CardFront);
+                    console.log(cardData.value.Priority);
                 }
                 else{
                     setCardFront("Queue Finished");
                     setVisiblityReveal("d-none");
                 }
             })
-        
-        await axios.post("http://localhost:3001/update-card-priority", {
-                withCredentials: true,
-                priority: newPriority,
-                deckID: deckID.deckid,
-                cardID: cardID
-            });
+    
     }
 
-    const handleClick = () => {
+    const updateCardPriority = async (newPriority) => {
+        dequeueCard();
+        await axios.post("http://localhost:3001/update-card-priority", {
+            withCredentials: true,
+            priority: newPriority,
+            deckID: cardData.value.DeckID,
+            cardID: cardData.value.CardID
+        });
+    }
+    
+
+    const showCardFront = () => {
+        setVisibilityOptions("d-none");
+        setRevealBack(false);
+        setVisiblityReveal("");
+    }
+
+
+    const showCardBack = () => {
         setRevealBack(true);
         setVisiblityReveal("d-none");
         setVisibilityOptions("");
@@ -72,20 +82,20 @@ const ReviewQueue = () => {
         <>
             <NavigationBar />
             <div className={styles.priorityDiv}>
-                Priority: {priority}
+                Priority: {cardData.value.Priority}
             </div>
             <div className={styles.cardContainer}>
                 <Card className={styles.reviewCard}>
                     <Card.Body>
                         <Card.Title>{revealBack ? cardBack : cardFront}</Card.Title>
                     </Card.Body>
-                    <Button onClick={handleClick} className={visibilityReveal}>Reveal Back</Button>
+                    <Button onClick={showCardBack} className={visibilityReveal}>Reveal Back</Button>
                     {["Easy", "Good", "Challenging", "Hard"].map((ease, index) => {
                         return(
                             <div className={styles.visibilityContainer}>
                                 <div className={visibilityOptions}>
                                     <div className={styles.easeButtons}>
-                                        <Button value={index} onClick={(e) => dequeueCard(e.target.value)}>{ease}</Button>
+                                        <Button value={index} onClick={(e) => updateCardPriority(e.target.value)}>{ease}</Button>
                                     </div>
                                 </div>
                             </div>
